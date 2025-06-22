852d615f784be6aeef7b17e02eb87e25fa9722b6
Hans Deol <142961894+HansD123@users.noreply.github.com>
2025-06-23 00:56:22 +0100

    Added electron specific filter optimisation pbs script

---
+++ pbs_scripts/custom_electron_op.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -l walltime=21:0:0
+#PBS -l select=1:ncpus=127:mem=256gb
+#PBS -N custom_op
+#PBS -o logs/
+#PBS -e logs/
+
+# Navigate to the directory where the SIF file is located
+cd $PBS_O_WORKDIR
+cd ../
+
+# Run the Apptainer container and execute the subsequent commands
+apptainer exec apptainer.sif /bin/bash -c "
+  source /root/.bashrc
+  cd src
+  python3 custom_electron_op.py > ../logs/custom_electron_op.log 2>&1
+"
